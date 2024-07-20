be114fc00935ee7fe20171216072afca778dc171
je-pa <jieun0502@gmail.com>
2024-07-20 16:44:39 +0900

    🎉 Begin a project

---
+++ goodsending/src/main/java/com/goodsending/GoodsendingApplication.java
@@ -0,0 +1,13 @@
+package com.goodsending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsendingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoodsendingApplication.class, args);
+  }
+
+}
---
+++ goodsending/src/test/java/com/goodsending/GoodsendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodsending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsendingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
