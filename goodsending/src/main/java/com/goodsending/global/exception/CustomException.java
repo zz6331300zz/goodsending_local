bf044b80ab12085f9d0de25c48b5928394f2f272
je-pa <76720692+je-pa@users.noreply.github.com>
2024-07-23 12:45:13 +0900

    ♻️ 공통기능 - Custom Exception 리펙토링 (#9)
    * ♻️ [#2] 메서드명 수정
    
    * ♻️ [#2] Exception 핸들러 리펙토링
    
    * ♻️ [#2] 커스텀 Exception 리펙토링

--- goodsending/src/main/java/com/goodsending/global/exception/CustomException.java
+++ goodsending/src/main/java/com/goodsending/global/exception/CustomException.java
@@ -1,11 +1,13 @@
 package com.goodsending.global.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public class CustomException extends RuntimeException{
+  @Getter
   private final HttpStatus statusCode;
 
-  public static CustomException of(ExceptionCode exceptionCode) {
+  public static CustomException from(ExceptionCode exceptionCode) {
     return new CustomException(exceptionCode.getStatus(), exceptionCode.getMessage());
   }
 
@@ -14,7 +16,4 @@ public class CustomException extends RuntimeException{
     this.statusCode = statusCode;
   }
 
-  public HttpStatus getStatusCode() {
-    return statusCode;
-  }
 }
