b2cd412bd028c0b0fec679f40efecd28029b18b8
zz6331300zz <zz6331300zz@gmail.com>
2024-08-09 15:52:16 +0900

    ✨[#74] 찜하기 수 top 5 RedisRankingRepository 변경

--- goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
+++ goodsending/src/main/java/com/goodsending/global/redis/RedisRankingRepository.java
@@ -1,11 +1,9 @@
 package com.goodsending.global.redis;
 
-import java.time.Duration;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
-
 
 @RequiredArgsConstructor
 public abstract class RedisRankingRepository<K, V> {
@@ -16,10 +14,6 @@ public abstract class RedisRankingRepository<K, V> {
   public void setZSetValue(K key, V value, double score) {
     redisTemplate.opsForZSet()
         .add(PREFIX + key, value, score);
-  }
-
-  public void setExpire(K key, int expireMinutes) {
-    redisTemplate.expire(PREFIX + key, Duration.ofMinutes(expireMinutes));
   }
 
   public void setHashValue(K key, String hashKey, V value) {
@@ -35,11 +29,27 @@ public abstract class RedisRankingRepository<K, V> {
     return redisTemplate.opsForZSet().rangeWithScores(PREFIX + key, start, end);
   }
 
+  public Set<TypedTuple<V>> getReverseZSetTupleByKey(K key, long start, long end) {
+    return redisTemplate.opsForZSet().reverseRangeWithScores(PREFIX + key, start, end);
+  }
+
+  public boolean isExistInRedis(String key, V value) {
+    Double score = redisTemplate.opsForZSet().score(PREFIX + key, value);
+    return score != null;
+  }
+
+  public void increaseScore(K key, V value, int delta) {
+    redisTemplate.opsForZSet().incrementScore(PREFIX + key, value, delta);
+  }
+
+  public void deleteZSetKey(K key) {
+    redisTemplate.delete(PREFIX + key);
+  }
+
   public void deleteZSetValue(K key) {
     Long size = redisTemplate.opsForZSet().size(PREFIX + key);
     if (size != null && size > 0) {
       redisTemplate.opsForZSet().removeRange(PREFIX + key, 0, size - 1);
     }
   }
-
 }
